642f1c28b4ad5ea277dbc709c8a15f5832f74ca3
ProSavage <naman0311@gmail.com>
2019-02-12 22:08:50 -0500

    Nav Bar improvements!

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default class TitleBar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            backgroundColor: '',
+        };
+    }
     renderItems() {
         if (this.props.items.length !== 0) {
             return this.props.items.map((item) => {
@@ -8,9 +14,34 @@ export default class TitleBar extends React.Component {
             });
         }
     }
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll.bind(this), { passive: true })
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll.bind(this))
+    }
+
+    handleScroll (event) {
+        this.setState({
+            backgroundColor: 'black'
+        });
+        if (window.pageYOffset === 0) {
+            this.setState({
+                backgroundColor: ''
+            });
+        }
+    }
     render() {
+
+        const styles = {
+            navBarStyle: {
+                backgroundColor: this.state.backgroundColor,
+            }
+        };
+        const { navBarStyle } = styles;
         return (
-          <div className="nav-bar">
+          <div className="nav-bar" style={navBarStyle}>
               <ul>
                   {this.renderItems()}
               </ul>
